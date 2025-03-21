c7e7e9642c67f793964247db0793246c02d0a071
shubhenduPandey04 <shubhendupandey28@gmail.com>
2025-03-21 22:42:07 +0530

    Updated Main.java

--- Method Overriding/Main.java
+++ Method Overriding/Main.java
@@ -1,11 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-//        PizzaOrder order = new PizzaOrder();
-//        // Different ways to order pizza (method overloading in action)
-//        order.orderPizza("Medium");
-//        order.orderPizza("Large", "Extra Cheese");
-//        order.orderPizza("Small", "Pepperoni", "Thin Crust");
 
         Food food = new Food();
         food.prepare(); // Output: Preparing generic food.
